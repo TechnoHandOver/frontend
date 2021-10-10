3f4bd70174af36cdee30ed23f8e6f8e74a02857e
nathanKith <ntolparov@gmail.com>
2021-10-10 20:47:43 +0300

    fix: map fix

--- src/components/DeliveryAdsList/DeliveryAdsList.jsx
+++ src/components/DeliveryAdsList/DeliveryAdsList.jsx
@@ -48,6 +48,7 @@ export const DeliveryAdsList = function DeliveryAdsList() {
                 setCards(data);
             })
             .catch(() => {
+                console.log(cards);
                 console.log('рисую письки');
             });
     }, [])
