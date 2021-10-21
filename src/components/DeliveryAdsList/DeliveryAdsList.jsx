2ee5ae83c2e47aa9bdb4618de800cd0fe141b7b0
nathanKith <ntolparov@gmail.com>
2021-10-22 00:04:32 +0300

    feat: added new panel

--- src/components/DeliveryAdsList/DeliveryAdsList.jsx
+++ src/components/DeliveryAdsList/DeliveryAdsList.jsx
@@ -4,52 +4,52 @@ import {CardGrid, Footer} from "@vkontakte/vkui";
 import {DeliveryAd} from "../DeliveryAd/DeliveryAd";
 import {customFetch} from "../../helpers/customFetch/customFetch";
 
-// const cards = [
-//     {
-//         locationFrom: 'Общежитие №4 Измайлово',
-//         locationTo: 'ГЗ МГТУ им Н.Э. Баумана',
-//         minPrice: '500',
-//         timeFrom: '2021-10-02T12:00:00.000Z',
-//         timeTo: '2021-10-02T12:20:00.000Z',
-//     },
-//     {
-//         locationFrom: 'Общежитие №4 Мытищи',
-//         locationTo: 'УЛК',
-//         minPrice: '350',
-//         timeFrom: '2021-10-02T12:00:00.000Z',
-//         timeTo: '2021-10-02T12:20:00.000Z',
-//     },
-//     {
-//         locationFrom: 'Общежитие №4 Мытищи',
-//         locationTo: 'ГЗ МГТУ им Н.Э. Баумана',
-//         minPrice: '370',
-//         timeFrom: '2021-10-02T12:00:00.000Z',
-//         timeTo: '2021-10-02T12:20:00.000Z',
-//     },
-//     {
-//         locationFrom: 'Общежитие №10 (СМ)',
-//         locationTo: 'ГЗ МГТУ им Н.Э. Баумана',
-//         minPrice: '370',
-//         timeFrom: '2021-10-02T12:00:00.000Z',
-//         timeTo: '2021-10-02T12:20:00.000Z',
-//     }
-// ]
+const mocks = [
+    {
+        locationFrom: 'Общежитие №4 Измайлово',
+        locationTo: 'ГЗ МГТУ им Н.Э. Баумана',
+        minPrice: '500',
+        timeFrom: '2021-10-02T12:00:00.000Z',
+        timeTo: '2021-10-02T12:20:00.000Z',
+    },
+    {
+        locationFrom: 'Общежитие №4 Мытищи',
+        locationTo: 'УЛК',
+        minPrice: '350',
+        timeFrom: '2021-10-02T12:00:00.000Z',
+        timeTo: '2021-10-02T12:20:00.000Z',
+    },
+    {
+        locationFrom: 'Общежитие №4 Мытищи',
+        locationTo: 'ГЗ МГТУ им Н.Э. Баумана',
+        minPrice: '370',
+        timeFrom: '2021-10-02T12:00:00.000Z',
+        timeTo: '2021-10-02T12:20:00.000Z',
+    },
+    {
+        locationFrom: 'Общежитие №10 (СМ)',
+        locationTo: 'ГЗ МГТУ им Н.Э. Баумана',
+        minPrice: '370',
+        timeFrom: '2021-10-02T12:00:00.000Z',
+        timeTo: '2021-10-02T12:20:00.000Z',
+    },
+];
 
 
 const backendUrl = 'https://handover.space';
 
 export const DeliveryAdsList = function DeliveryAdsList() {
-    const [cards, setCards] = React.useState([]);
+    const [cards, setCards] = React.useState(mocks);
 
-    React.useEffect(() => {
-        customFetch(`${backendUrl}/api/ads/list`)
-            .then(({data}) => {
-                setCards(data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, [])
+    // React.useEffect(() => {
+    //     customFetch(`${backendUrl}/api/ads/list`)
+    //         .then(({data}) => {
+    //             setCards(data);
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+    // }, [])
 
     return (
         <CardGrid size='l'>
--- src/components/NavigationPanel/NavigationPanel.jsx
+++ src/components/NavigationPanel/NavigationPanel.jsx
@@ -1,35 +1,7 @@
 import * as React from 'react';
-import {FixedLayout, IconButton, Separator} from "@vkontakte/vkui";
-import {Icon16LikeOutline, Icon20Add, Icon20Search, Icon28Profile} from '@vkontakte/icons';
+import {Caption, FixedLayout, IconButton} from '@vkontakte/vkui';
+import { iconIds, icons } from './NavigationPanel.config';
 import './NavigationPanel.css';
-
-const defaultIconSizes = 28;
-const iconIds = {
-    'add': 'createads',
-    'search': 'adsListPage',
-}
-const icons = [
-    {
-        id: 'like',
-        content: <Icon16LikeOutline width={defaultIconSizes} height={defaultIconSizes}/>,
-        activeContent: <Icon16LikeOutline width={defaultIconSizes} height={defaultIconSizes}  fill='#5c9ce6'/>,
-    },
-    {
-        id: 'profile',
-        content: <Icon28Profile/>,
-        activeContent: <Icon28Profile fill='#5c9ce6'/>,
-    },
-    {
-        id: 'add',
-        content: <Icon20Add width={defaultIconSizes} height={defaultIconSizes}/>,
-        activeContent: <Icon20Add width={defaultIconSizes} height={defaultIconSizes} fill='#5c9ce6'/>,
-    },
-    {
-        id: 'search',
-        content: <Icon20Search width={defaultIconSizes} height={defaultIconSizes}/>,
-        activeContent: <Icon20Search width={defaultIconSizes} height={defaultIconSizes} fill='#5c9ce6'/>,
-    }
-];
 
 export const NavigationPanel = function NavigationPanel({active, navigationHandler}) {
     const [activeId, setActiveId] = React.useState(active);
@@ -46,12 +18,14 @@ export const NavigationPanel = function NavigationPanel({active, navigationHandl
         <FixedLayout filled vertical='bottom'>
             <div className='navigational-panel'>
                 {icons.map(item => (
-                    <IconButton id={item.id} onClick={onClickHandler} key={item.id}>
-                        {iconIds[item.id] === activeId ? item.activeContent : item.content}
-                    </IconButton>
-                ))}
+                    <div className="icon-button">
+                        <IconButton id={item.id} onClick={onClickHandler} key={item.id}>
+                            {iconIds[item.id] === activeId ? item.activeContent : item.content}
+                        </IconButton>
+                        <Caption level="1" weight="regular" className="icon-button__text" style={{ color: iconIds[item.id] === activeId ? '#4a86cc' : '#99a2ad' }}>{item.text}</Caption>
+                    </div>
+                    ))}
             </div>
         </FixedLayout>
-
     );
 }
