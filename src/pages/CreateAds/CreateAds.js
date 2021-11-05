aae0806963c18bc72a3d98b4bb98ab79b39e9d1c
nathanKith <ntolparov@gmail.com>
2021-11-05 19:01:56 +0300

    fix: types

--- src/pages/CreateAds/CreateAds.js
+++ src/pages/CreateAds/CreateAds.js
@@ -60,7 +60,7 @@ export const CreateAds = ({ id, navigationHandler, active, setPopout, setActiveP
             locationFrom,
             locationTo,
             new Intl.DateTimeFormat('ru', {day: '2-digit', month: '2-digit', year: 'numeric'}).format(now),
-            price,
+            Number(price),
             extra,
             subject
         )
